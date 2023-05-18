27c8c6e26717b96472940a52cfb49c15f1cd68e2
Alejandro Grijalba <117680299+alejogrlbaz@users.noreply.github.com>
2023-05-18 16:55:35 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+// Definir una función que salude al usuario
+function saludar(nombre) {
+  console.log("¡Hola, " + nombre + "!");
+}
+
+// Llamar a la función y pasarle un nombre
+saludar("Alejandro");
